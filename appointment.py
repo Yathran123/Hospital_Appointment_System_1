134459d9c3601d1cafae25e2afef8054a46936db
Yathran123 <yathran.r2024@vit.ac.in>
2026-07-19 12:15:49 +0530

    Added login feature

--- appointment.py
+++ appointment.py
@@ -4,3 +4,8 @@ Doctor ="Dr.Yathran"
 print("Patient Name:", Patient_name)
 print("Doctor:",Doctor)
 print("Appointment Registered Successfully")
+username = "Yathran123"
+password = "12345"
+print("Login Feature Added")
+print("Username:", username)
+print("Password:", password)
