605f82f8574fc37f0c6cba1cdfea12de56a50289
Yathran123 <yathran.r2024@vit.ac.in>
2026-07-19 11:43:05 +0530

    Added patient registration module

--- appointment.py
+++ appointment.py
@@ -1,1 +1,6 @@
 print("Hospital Appointment Booking System")
+Patient_name ="Deeksha"
+Doctor ="Dr.Yathran"
+print("Patient Name:", Patient_name)
+print("Doctor:",Doctor)
+print("Appointment Registered Successfully")
